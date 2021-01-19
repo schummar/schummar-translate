0c7e50b5857c771eca109d1a713baeb20c20901a
Marco Schumacher <marco.schumacher@pentlandfirth.com>
2021-01-19 16:23:46 +0100

    Added hooks.

--- src/createTranslator.tsx
+++ src/createTranslator.tsx
@@ -3,6 +3,7 @@ import { GetICUArgs } from './extractICU';
 import Translation from './translation';
 import { Translator } from './translator';
 import { DeepValue, Dict, FlatKeys, Options } from './types';
+import { useTranslate as innerUseTranslate } from './useTranslate';
 
 type Values<D extends Dict, K extends string> = GetICUArgs<DeepValue<D, K>>;
 type Args<D extends Dict, K extends string> = Record<string, never> extends Values<D, K>
@@ -12,15 +13,31 @@ type Args<D extends Dict, K extends string> = Record<string, never> extends Valu
 export function createTranslator<D extends Dict>(
   options: Options<D>,
 ): {
-  translator: Translator<D>;
   t: <K extends FlatKeys<D>>(id: K, ...args: Args<D, K>) => JSX.Element;
+  useTranslate: <K extends FlatKeys<D>>(locale?: string) => (id: K, ...args: Args<D, K>) => string;
+  useTranslation: <K extends FlatKeys<D>>(id: K, ...args: Args<D, K>) => string;
 } {
   const translator = new Translator(options);
 
+  function t<K extends FlatKeys<D>>(id: K, ...args: Args<D, K>) {
+    return <Translation translator={translator} id={id} values={args[0]} fallback={args[1]} />;
+  }
+
+  function useTranslate(locale?: string) {
+    const translate = innerUseTranslate(translator, locale);
+    return function <K extends FlatKeys<D>>(id: K, ...args: Args<D, K>) {
+      return translate({ id, ...args });
+    };
+  }
+
+  function useTranslation<K extends FlatKeys<D>>(id: K, ...args: Args<D, K>) {
+    const translate = useTranslate();
+    return translate(id, ...args);
+  }
+
   return {
-    translator,
-    t: <K extends FlatKeys<D>>(id: K, ...args: Args<D, K>) => (
-      <Translation translator={translator} id={id} values={args[0]} fallback={args[1]} />
-    ),
+    t,
+    useTranslate,
+    useTranslation,
   };
 }
