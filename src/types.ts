712b34194cd6a592e245eae67be46545c4e39176
Marco Schumacher <marco@schumacher.dev>
2021-06-14 08:03:16 +0200

    Small optimization.

--- src/types.ts
+++ src/types.ts
@@ -6,21 +6,16 @@ export type MaybePromise<T> = T | Promise<T>;
 /** Merge tuple into intersection */
 export type Merge<T> = T extends [infer A, ...infer Rest] ? (Rest extends [any, ...any] ? A & Merge<Rest> : A) : T;
 
-type Str<T> = T extends string ? T : never;
-
-export type FlatKeys<T extends Record<string, unknown>> = Str<
+export type FlatKeys<T extends Record<string, unknown>> = string &
   keyof {
-    [K in Str<keyof T> as T[K] extends Record<string, unknown> ? `${K}.${FlatKeys<T[K]>}` : K]: 1;
-  }
->;
+    [K in keyof T as T[K] extends Record<string, unknown> ? `${string & K}.${FlatKeys<T[K]>}` : K]: 1;
+  };
 
 export type DeepValue<T extends Record<string, unknown>, K extends string> = K extends `${infer Head}.${infer Rest}`
   ? T[Head] extends Dict
     ? DeepValue<T[Head], Rest>
     : never
   : T[K];
-
-export type FlattenDict<T extends Record<string, unknown>> = { [K in FlatKeys<T>]: DeepValue<T, K> };
 
 /** Make all nested properties optional */
 export type PartialDict<T extends Dict> = {
